84c1e0c265124a42c16a20d0af30e5e4c18d9ae6
Rediet_W <vivezlaviequetuaimes@gmail.com>
2024-12-20 20:03:29 +0300

    working on deployment auth issues

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,19 +3,36 @@ import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
+  let token = req.cookies.jwt; // Assuming the JWT is stored in a cookie
+  console.log("JWT Cookie:", token);
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log(decoded, "decode");
+      // Fetch user by ID from the MySQL database using Sequelize
+      const user = await User.findByPk(decoded.userId); // Find user by primary key (ID)
 
-  // Check for token in the Authorization header or cookies
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1]; // Extract token from Authorization header
-  } else if (req.cookies.jwt) {
-    token = req.cookies.jwt; // Extract token from cookies
-  }
+      if (!user) {
+        res.status(401);
+        throw new Error("Not authorized, user not found");
+      }
 
-  if (!token) {
+      // Attach the user object to the request
+      req.user = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        isPrimaryAdmin: user.is_primary_admin,
+      };
+      console.log("User Attached to Request:", req.user);
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  } else {
     res.status(401);
     throw new Error("Not authorized, no token");
   }
--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,11 +1,23 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://inventory-management-system-mk96.onrender.com/",
+  baseUrl: "https://inventory-management-system-mk96.onrender.com",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    // Retrieve userInfo from the Redux state
+    const userInfo = getState().auth.userInfo;
+    console.log("Redux User Info:", getState().auth.userInfo);
+    // If a token exists in userInfo, set it in the Authorization header
+    if (userInfo?.token) {
+      headers.set("Authorization", `Bearer ${userInfo.token}`);
+    }
+
+    return headers;
+  },
 });
 
 export const apiSlice = createApi({
   baseQuery,
-  tagTypes: ["User"],
-  endpoints: (builder) => ({}),
+  tagTypes: ["User", "RequestedProduct"], // Add other tags as needed
+  endpoints: (builder) => ({}), // Define your endpoints here
 });
