272439b355cbb111d5488093b63351ae68943714
Rediet Woudma <139815109+Rediet-W@users.noreply.github.com>
2024-12-26 11:37:30 +0300

    logging the backend url

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,5 +1,6 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
+console.log("Backend URL:", backendUrl);
 const baseQuery = fetchBaseQuery({
   baseUrl: backendUrl,
   credentials: "include",
